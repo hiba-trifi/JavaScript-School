f119bcf6b339f5abf7821233c48493ee1523228a
Hiba Trifi <trifi.hiba.solicode@gmail.com>
2022-11-15 12:22:52 +0100

    Brief 4

---
+++ script.js
@@ -0,0 +1,612 @@
+   //variable de sow and hide exercices /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var navbtn = document.getElementById('navbtn')
+   var navbarmobile = document.getElementById('navbarmobile')
+   var ex1 = document.getElementById('ex1')
+   var ex2 = document.getElementById('ex2')
+   var ex3 = document.getElementById('ex3')
+   var ex4 = document.getElementById('ex4')
+   var ex5 = document.getElementById('ex5')
+   var ex6 = document.getElementById('ex6')
+   var ex7 = document.getElementById('ex7')
+   var ex8 = document.getElementById('ex8')
+   var ex9 = document.getElementById('ex9')
+   var ex10 = document.getElementById('ex10')
+   var home =  document.getElementById('home')
+   var x = window.matchMedia("(max-width: 360px)")
+   
+    function navshow(){
+    navbtn.style.display='none' 
+    navbarmobile.style.display='block'
+   }
+
+   function homepage(){
+    navbtn.style.display='block'
+    home.style.display='block' 
+    ex1.style.display='none' 
+    ex2.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+        if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+        
+       }
+
+   function showex1(){
+    ex1.style.display='inline' 
+    ex2.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+}
+
+
+function showex2(){
+ ex2.style.display='inline' 
+ ex1.style.display='none'
+ ex3.style.display='none'
+ ex4.style.display='none'
+ ex5.style.display='none'
+ ex6.style.display='none'
+ ex7.style.display='none'
+ ex8.style.display='none'
+ ex9.style.display='none'
+ ex10.style.display='none'
+ home.style.display='none' 
+ if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+ navbtn.style.display='block'
+}
+
+
+function showex3(){
+    ex3.style.display='inline' 
+    ex1.style.display='none'
+    ex2.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+
+   
+
+   function showex4(){
+    ex4.style.display='inline' 
+    ex1.style.display='none'
+    ex3.style.display='none'
+    ex2.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+
+   
+
+
+   function showex5(){
+    ex5.style.display='inline' 
+    ex1.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex2.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+
+   
+
+
+   function showex6(){
+    ex6.style.display='inline' 
+    ex1.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex2.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+
+   
+
+
+   function showex7(){
+    ex7.style.display='inline' 
+    ex1.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex2.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+
+   
+
+
+   function showex8(){
+    ex8.style.display='inline' 
+    ex1.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex2.style.display='none'
+    ex9.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+
+   
+
+
+   function showex9(){
+    ex9.style.display='inline' 
+    ex1.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex2.style.display='none'
+    ex10.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+
+   
+   
+
+
+   function showex10(){
+    ex10.style.display='inline' 
+    ex1.style.display='none'
+    ex3.style.display='none'
+    ex4.style.display='none'
+    ex5.style.display='none'
+    ex6.style.display='none'
+    ex7.style.display='none'
+    ex8.style.display='none'
+    ex9.style.display='none'
+    ex2.style.display='none'
+    home.style.display='none' 
+    if (x.matches) { 
+           navbarmobile.style.display='none';
+        } else {
+           navbarmobile.style.display='block'
+        }
+    navbtn.style.display='block'
+   }
+   
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// hide and show sous nav////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var sous1 = document.getElementById('sous1')
+var sous2 = document.getElementById('sous2')
+var sous3 = document.getElementById('sous3')
+var color1 = document.getElementById('color1')
+var color2 = document.getElementById('color2')
+var color3 = document.getElementById('color3')
+
+   // show//
+function shownav1()
+{
+   sous1.style.display='inline';
+   color1.style.backgroundColor = "#3b3b3b"
+
+}
+function shownav2()
+{
+   sous2.style.display='inline'
+   color2.style.backgroundColor = "#3b3b3b"
+   
+}
+function shownav3()
+{
+   sous3.style.display='inline'
+   color3.style.backgroundColor = "#3b3b3b"
+}
+  // hide //
+  function hidenav1()
+  {
+    sous1.style.display='none'
+     color1.style.backgroundColor = "#232425"
+  }
+  function hidenav2()
+  {
+    sous2.style.display='none'
+     color2.style.backgroundColor = "#232425"
+  }
+  function hidenav3()
+  {
+    sous3.style.display='none'
+     color3.style.backgroundColor = "#232425"
+  }
+
+
+
+// permutation function //////////////////////////////////////////////////////////////////////////////////////////////
+
+const a = document.getElementById("a");
+const b = document.getElementById("b");
+
+function permuter() {
+
+x=document.form.a.value;
+document.form.a.value=document.form.b.value;
+document.form.b.value=x;
+} 
+
+
+// Validation de date fonction //////////////////////////////////////////////////////////////////////////////////////
+var jour = document.getElementById('jour');
+var mois = document.getElementById('mois');
+var annes = document.getElementById('annes');
+var validation = document.getElementById('validation');
+var validDtae = document.getElementById('validDtae');
+
+function checkDate()
+{
+    if ( (annes.value > 1900 && annes.value < 2025)
+        &&
+         jour.value>=1  
+        &&
+         (((mois.value == 4 || mois.value == 6 || mois.value == 9 || mois.value == 11) && jour.value <= 30)
+          || 
+         ((mois.value == 1 || mois.value == 3 || mois.value == 5 || mois.value == 7 || mois.value == 8 || mois.value == 10 || mois.value == 12) && jour.value <= 31)
+          ||
+        (mois.value == 2 && jour.value <= 28)) )
+    {
+        validation.innerHTML=`La date ${jour.value} / ${mois.value} / ${annes.value} est Valide `
+    }
+    else 
+    {
+        validation.innerHTML=`Cette date est Unvalide`
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// multiplication function /////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ function multiplication() {
+    var number=document.getElementById("number").value;
+    var multResult = document.getElementById("result") 
+     multResult.innerHTML = "";
+   for(i=1;i<=10;i++){
+    multResult.innerHTML += number+ " " + 'X' + " " + i + " " + '=' + " " +number*i +`<br>` ;
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// deviner un nombre function ////////////////////////////////////////////////////////////////////////////////////////////////
+var playagain = document.getElementById('playagain')
+var guessnum  = document.getElementById('guessnum');
+var message = document.getElementById('message');
+var guessBtn = document.getElementById('guessBtn')
+var counter = 0
+var random =  Math.floor(Math.random()* 50+1);
+    function guess(){
+        counter++; 
+        tentatives.innerHTML = counter;
+    if ( guessnum.value == ""){
+        alert("S'il vous plaît, saisir un nombre!")
+    }
+    else if ( counter == 10){
+        guessBtn.disabled = true ;
+        message.innerHTML= 'Jeux tèrminer';
+        playagain.style.display='inline';    
+    }    
+    else if (guessnum.value ==  random) {
+        message.innerHTML= "félicitation, tu as gagné après " + counter + " " + "tentatives"
+        counter=0; 
+     }  
+    else if(guessnum.value <  random) {
+            message.innerHTML=  "Le nombre est plus grand que " + "  " + guessnum.value + "  " + " , Réesseyez encore !" 
+        }
+    else if (guessnum.value >  random) {
+            message.innerHTML=  "Le nombre est plus petit que " + "  " + guessnum.value + "  " + " , Réesseyez encore !"
+        } 
+    }
+    function reload(){
+        location.reload();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// losange function /////////////////////////////////////////////////////////////////////////////////////////
+
+function stardraw() {
+    var drawInp = document.getElementById("drawInp");
+    var drawing = document.getElementById("drawing");
+    var i, k;
+         drawing.innerHTML="";
+    for (i = 1; i <= drawInp.value ; i++) 
+    {
+    for (k = 1; k <= i; k++) {
+    drawing.innerHTML += "*";
+    }
+    drawing.innerHTML += "<br>";
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//calculatrice functions //////////////////////////////////////////////////////////////////////////////////////////
+    function plus()
+    {
+        var a,b,c;
+        a=parseInt(document.getElementById("num1").value);
+        b=parseInt(document.getElementById("num2").value);
+        c= a + b;
+        document.getElementById("answer").value= c;
+    }
+    function moin()
+    {
+        var a,b,c;
+        a=parseInt(document.getElementById("num1").value);
+        b=parseInt(document.getElementById("num2").value);
+        c= a - b;
+        document.getElementById("answer").value= c;
+   }
+   function mul()
+   {
+    var a,b,c;
+    a=parseInt(document.getElementById("num1").value);
+    b=parseInt(document.getElementById("num2").value);
+    c= a * b;
+    document.getElementById("answer").value= c;
+    }
+    function dev()
+    {
+        var a,b,c;
+        a=parseInt(document.getElementById("num1").value);
+        b=parseInt(document.getElementById("num2").value);
+        c= a / b;
+        document.getElementById("answer").value= c;
+    }
+
+// voyel function /////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function count()
+{  var voyelinp = document.getElementById('strInp').value;
+   let counter = 0;  
+   let result = voyelinp.match(/[aeoiyuAEYUIO]/g);
+for (let i = 0; i < result.length; i++)
+     counter++
+    document.getElementById("voyelnum").innerHTML = 'Le nombre de voyelle dans cette phrase est : '+counter;
+}
+
+// rechercher et suprimer /////////////////////////////////////////////////////////////////////////////////////////////////
+var text = document.getElementById('text');
+var mot = document.getElementById('mot');
+var outputWord = document.getElementById('outputWord');
+    //rechercher//
+function rechercher(){ 
+    let motval = mot.value
+    let textval = text.value;
+    if (textval.match(motval))   {outputWord.innerHTML = "Ce mot existe"}
+    else  {outputWord.innerHTML = "Ce mot n'existe pas"}
+}
+// suprimmer //
+function suprimer(){
+    let motval = mot.value
+    let textval = text.value;
+while (textval.match(motval))   {textval = textval.replace(motval,"");}
+    outputWord.innerHTML = textval
+}
+
+// tableau tri  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function tritablecreat(){
+    var inputs = document.getElementById('inputs')
+    var taille = parseInt(document.getElementById('taille').value)
+    inputs.innerHTML="";
+    for(x=1 ; x<= taille ;x++){
+        var createdinp = document.createElement('INPUT')
+        createdinp.setAttribute('id', 'input'+x)
+        inputs.appendChild(createdinp)
+    }}
+
+
+var outsort = document.getElementById('sortedarray')
+function tri(){
+const array = [] 
+var taille = parseInt(document.getElementById('taille').value)
+    for (let x = 1; x<= taille; x++)
+    { array.push(parseInt(document.getElementById('input'+x).value))} 
+    array.sort(function(a,b){ return a-b});
+    outsort.innerHTML = array
+}
+
+
+    // tableau recherche ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   function reverse(){
+        let inp1="";
+          let tailleinp=document.getElementById('tailleinp').value ;
+            for (let i = 0; i <tailleinp;i++) {
+              inp1=inp1+"<input id=en"+i+">"
+              document.getElementById('output1').innerHTML=inp1
+            }
+          }
+   
+    function recherchetabl() {
+        var finalout = document.getElementById('finalout')
+          let valeurArechercher=document.getElementById('valeurArechercher').value;
+          let inp2="";
+          for(let j=0 ; j<=10 ; j++){
+            inp2=inp2+" "+document.getElementById("en"+j).value
+            if(inp2.includes(valeurArechercher)){
+           finalout.innerHTML=" La valeur " + valeurArechercher +" existe dans le tableau "
+            }
+            else {
+             finalout.innerHTML="La  valeur "+ valeurArechercher +" n'existe pas dans le tableau "
+            }
+          }
+      }
